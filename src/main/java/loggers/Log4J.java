3fc8c58089c3a2a418ae6f26a9790e0e740e1152
sl <radut01@gmail.com>
2019-12-03 20:54:01 +0300

    Add new logger class

---
+++ src/main/java/loggers/Log4J.java
@@ -0,0 +1,16 @@
+package loggers;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Log4J {
+    private Logger logger;
+    private String msg;
+
+    void doStnk(){
+        msg = "Logger";
+         logger.getLogger("Log4J").log(Level.INFO,msg);
+    }
+
+}
